7e2dc7ca23f6d1a0c7bbe348c5e9c8a21f2f433b
lewismck <lewisamckeown@gmail.com>
2017-11-06 21:51:45 +0000

    Some tidying up of the 10Print code

--- 10Print/sketch.js
+++ 10Print/sketch.js
@@ -3,14 +3,15 @@
  * https://github.com/CodingTrain/Rainbow-Code/blob/master/CodingChallenges
  **/
 
-let x = 0;
-let y = 0;
+let x = 0; //LET  LET  LET
+let y = 0; //ECMA ECMA ECMA
 
 
 
  function setup(){
    createCanvas(800,800);
    background(0);
+   //Add some sliders to control the spacing and pattern direction
    spaceSlide = createSlider(1, 100, 20, 1);
    probabilitySlide = createSlider(0, 1, 0.5, 0.1);
  }
@@ -18,15 +19,18 @@ let y = 0;
 
  function draw(){
    stroke(255);
-   let spacing = spaceSlide.value();
-   if(random(1) < probabilitySlide.value()){
-     line(x, y, x + spacing,y + spacing);
-   }
-   else{
-     line(x,y + spacing,x + spacing, y);
-   }
 
-   x = x + spacing;
+  let spacing = spaceSlide.value();
+
+  if(random(1) < probabilitySlide.value()){
+    line(x, y, x + spacing,y + spacing);
+  }
+  else{
+    line(x,y + spacing,x + spacing, y);
+  }
+
+  x = x + spacing;
+
   if (x > width){
     x = 0;
     y = y + spacing;
---
+++ FreekePattern/sketch.js
@@ -0,0 +1,13 @@
+/**
+ * https://blog.kadenze.com/creative-technology/p5-js-crash-course-recreate-art-you-love/
+ **/
+
+ function setup(){
+   createCanvas(800,800);
+   background(0);
+ }
+
+
+ function draw(){
+   
+ }
