887504011ac27189c460506687ddfea260a3d918
lewismck <lewisamckeown@gmail.com>
2018-08-28 17:08:56 +0100

    why not sort bezier curves also?

--- AlgorithmVisualisation/BubbleSort/sketch.js
+++ AlgorithmVisualisation/BubbleSort/sketch.js
@@ -5,13 +5,17 @@
  var values = [];
  var i = 0;
  var j = 0;
+ var lineType = 'line';
 
  function setup(){
    createCanvas(800,600);
-
+   //Create an array of semi random values
    for(var i =0; i < width; i++){
      values[i] = random(height);//Why not change the line generation? //noise(i/100*height);
    }
+
+   button = createButton('Switch');
+   button.mousePressed(switchLineType);
  }
 
 
@@ -19,31 +23,52 @@
    background(51);
    stroke(0);
 
-   drawLines();
+   //Which line to draw?
+   if(lineType == 'line'){
+     drawLines();
+   }else{
+    drawBezierCurves();
+   }
+
    a = values[j];
    b = values[j+1];
    if(a > b){
      swapValues(values, j, j+1);
    }
 
-   //Sort 100 times per frame
-   // for(var n = 0; n < 100; n++){
-     if(i < values.length){
-       j++;
-       if(j >= values.length-i-1){
-         j = 0;
-         i = i+1;
-       }
-     } else{
-       console.log("finished");
-       noLoop();
-     }
-   // }
+   //For the draw loop only increment if the array is not exhausted
+   if (i < values.length){
+      j++;
+      if(j >= values.length-i-1){
+        j = 0;
+        i = i+1;
+      }
+   } else {
+      console.log("finished");
+      noLoop();
+   }
+
+ }
+
+ function switchLineType(){
+   // console.log(lineType);
+   if(lineType == 'line'){
+     lineType = 'bezier';
+   } else {
+     lineType = 'line';
+   }
  }
 
  function swapValues(values, a, b){
    values[a] = b;
    values[b] = a;
+ }
+
+ function drawBezierCurves(){
+   for(var i = 0; i < values.length; i++){
+      bezier(i, height, i*0.1, i, i, height-values[i], i*0.1, i);
+     // bezier(i, height, i*0.2, noise(i/100*height), i, height-values[i], noise(i/100*height), i);
+   }
  }
 
  function drawLines(){
