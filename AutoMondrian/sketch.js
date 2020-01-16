50bffdea0e86468de90fd548bf04940753a329ec
lewismck <lewisamckeown@gmail.com>
2020-01-16 18:04:46 +0000

    Tidy up autoMondrian
    Draw rectangles in grid

--- AutoMondrian/sketch.js
+++ AutoMondrian/sketch.js
@@ -4,22 +4,44 @@
  **/
 var boxes = [];
 var saveButton;
+var c;
 
 var fills = [
   [255, 204, 0], //orange/yellow 
   [0, 0, 255], //blue 
   [255, 0, 0], //red
-  [0,0,0] //black
+  [255,255,255] //white
+  //[0,0,0] //black
 ];
 
 function setup() {
-  var c = createCanvas(window.innerWidth, 800);
-  
+  let c = createCanvas(window.innerWidth, 800);
+  let x=0;
+  let y=0;
   background('white');
+  
+  //height
+  let sqHeight = random(50, 500);
+  //width
+  let sqWidth = random(50, 500);
+  
   //Create a load of boxes with random border widths
-  //Fill some of them with red/yellow/blue
-  for(var i = 0; i < random(20, height/10); i++){
-      drawBox();
+  //Fill some of them with red/yellow/blue/white
+  //draw rects until we hit the end of the x and y axes
+  //based off of: https://editor.p5js.org/angichau/sketches/r1BOM69-4
+  while (y < height) {
+    x = 0;
+    
+    while (x < width) {     
+    	// fill(random(colors));
+      // rect(x,y,currWidth,currHeight);
+      let box = drawBox(x,y, sqHeight, sqWidth);
+      x = x+sqWidth;
+      sqWidth = random(50, 500);
+      // currWidth = random(sizes); 
+  	}
+    y = y+sqHeight;
+    sqHeight = random(50, 500);
   }
 
   //add useful buttons
@@ -36,24 +58,18 @@ function draw() {
  * Probably take x/y h/w params and write something to place the boxes in a sensible order on canvas
  * 
  */
-function drawBox(){
+function drawBox(canvX, canvY, sqHeight, sqWidth){
   //StrokeWidth
   strokeWeight(random(1,10));
   //random fill
   if(random(0,1) > 0.5){
     fill(fills[Math.floor(Math.random()*fills.length)]);
   }
-  //canvas x
-  let canvX = random(width);
-  //canvas y
-  let canvY = random(canvX +(random(-100, 100)));
-  //height
-  let sqHeight = random(50, 500);
-  //width
-  let sqWidth = random(50, 500);
   rect(canvX, canvY, sqWidth, sqHeight);
+  // return {'w': sqWidth, 'h': sqHeight};
 }
 
 function saveImage(){
   saveCanvas(c, 'auto-mondrian', 'jpg');
 }
+
